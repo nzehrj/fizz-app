bb191aa57065dfc72d09c4df09521cc364b5bb61
Rejoice Nzeh <rejoicenze477@gmail.com>
2025-05-17 12:54:14 +0100

    11-signin-user

--- lib/constants.ts
+++ lib/constants.ts
@@ -5,7 +5,9 @@ export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
   'An Ecommerce App built with Next.js, MongoDB, Shadcn'
 export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9)
-
+export const APP_COPYRIGHT =
+  process.env.NEXT_PUBLIC_APP_COPYRIGHT ||
+  `Copyright © 2025 ${APP_NAME}. All rights reserved.`
 export const FREE_SHIPPING_MIN_PRICE = Number(
   process.env.FREE_SHIPPING_MIN_PRICE || 35
 )
